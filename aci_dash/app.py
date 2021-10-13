0b9f58362a78068714184cc60767491bb97790a9
ba1 <ba1@users.noreply.github.com>
2021-10-13 22:20:24 +0200

    Update app.py

--- aci_dash/app.py
+++ aci_dash/app.py
@@ -61,9 +61,6 @@ except KeyError:
 
 version_number = os.getenv("VERSION", "-1")
 
-if not version_number:
-    version_number = "1.0.0"
-
 app_name = "Companion Dashboard - Acinetobacter Comparative Genomics - v" + version_number
 server = Flask(app_name)
 
